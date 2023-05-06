d1d9592c15d2bc423d8ae4e4bdc30d9101e2f48a
Abel Ibarra Solgorre <aibarrasol@outlook.com>
2023-05-06 17:15:29 -0500

    Clase-01: Completada

--- app.js
+++ app.js
@@ -19,5 +19,13 @@ cliente2.dniCliente = '148552679';
 cliente2.numeroCuenta = '4978-5525-9654-4125';
 cliente2.saldoCuenta = 7000;
 
+const cliente3 = new Cliente();
+
+cliente3.nombreCliente = 'Jose Condorcanqui Robles';
+cliente3.dniCliente = '74851689';
+cliente3.numeroCuenta = '8784-8755-8459-8465';
+cliente3.saldoCuenta = 10;
+
 console.log(cliente1);
 console.log(cliente2);
+console.log(cliente3);
